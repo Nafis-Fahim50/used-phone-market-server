4cc0e48ae0da706c18e99370f10604e9cf8090ea
Nafis-Fahim50 <nafisf96@gmail.com>
2022-11-24 16:31:20 +0600

    get categories data

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
@@ -14,6 +14,31 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.6llxg7j.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run(){
+    try{
+        const categoryCollection = client.db('resaleMarket').collection('categories');
+        const productCollection = client.db('resaleMarket').collection('products');
+
+        app.get('/categories', async(req, res)=>{
+            const query = {};
+            const cursor = categoryCollection.find(query);
+            const result = await cursor.toArray()
+            res.send(result);
+        })
+
+        app.get('/categories/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = {}
+            const products = await productCollection.find(query).toArray()
+            const phone_collection = products.filter(p => p.category_id == id)
+            res.send(phone_collection);
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(error => console.log(error))
 
 app.get('/', (req, res) =>{
     res.send('Resale Market Server is Running...')
